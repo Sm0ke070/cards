e607c56f489a6c73a4d6c2651773392bd2262bb7
Sm0ke070 <smokedeveloper070@gmail.com>
2023-02-05 00:40:23 +0300

    Login

--- src/features/auth/sign-in/Login.tsx
+++ src/features/auth/sign-in/Login.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import {authAPI} from "./login.api";
 import {SubmitHandler, useForm} from "react-hook-form";
 import {useAppDispatch, useAppSelector} from "../../../app/store";
 import {Navigate} from "react-router-dom";
