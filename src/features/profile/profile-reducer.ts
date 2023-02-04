34d72147e1250be11c98b862885de4c2be2907fe
Sm0ke070 <smokedeveloper070@gmail.com>
2023-02-04 13:28:33 +0300

    Удалил папку .idea из репозитория

---
+++ src/features/profile/profile-reducer.ts
@@ -0,0 +1,12 @@
+const initialState = {
+
+}
+type InitialStateType = typeof initialState
+
+export const ProfileReducer = (state: InitialStateType = initialState, action: any): InitialStateType => {
+    switch (action.type) {
+
+        default:
+            return state
+    }
+}
