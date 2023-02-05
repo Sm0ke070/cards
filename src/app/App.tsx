7507821a7a9a61b6f66db467edaf7f5ca57773af
Sm0ke070 <smokedeveloper070@gmail.com>
2023-02-05 17:09:03 +0300

    Nikita-added logout,authentication request

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import './App.css';
 import {Navigate, Route, Routes} from "react-router-dom";
 import Profile from "../features/profile/Profile";
@@ -8,17 +8,41 @@ import ResetPassword from "../features/password/reset-password/ResetPassword";
 import Test from "../common/components/super-components/Test";
 import Layout from "../common/components/Layout/Layout";
 import Login from "../features/auth/sign-in/Login";
+import {useAppDispatch, useAppSelector} from "./store";
+import {meTC, RequestStatusType} from "./app-reducer";
+import {logoutTC} from "../features/auth/sign-in/auth-reducer";
 
 const App = () => {
 
+    const dispatch = useAppDispatch()
+    const status = useAppSelector<RequestStatusType>((state) => state.app.status)
+    const isInitialized = useAppSelector((state) => state.app.isInitialized)
+    const isLoggedIn = useAppSelector((state) => state.auth.isLoggedIn)
+
+    useEffect(() => {
+        dispatch(meTC())
+    }, [])
+
+    const logout = () => {
+        dispatch(logoutTC())
+    }
+
+    if (!isInitialized) {
+        return <div
+            style={{position: 'fixed', top: '30%', textAlign: 'center', width: '100%'}}>
+            <h1>Loading</h1>
+        </div>
+    }
 
     return (
         <>
+            {/*кнопка тут временно*/}
+            {isLoggedIn && <button onClick={logout}>logOut</button>}
             <Routes>
                 <Route path={'/'} element={<Layout/>}>
                     <Route path={'/profile'} element={<Profile/>}/>
-                    <Route path={'/login'} element={<Login/>}/>
-                    <Route path={'/registration'} element={<Registration/>}/>
+                    <Route path={'/sign-in'} element={<Login/>}/>
+                    <Route path={'/sign-up'} element={<Registration/>}/>
                     <Route path={'/res_password'} element={<ResetPassword/>}/>
                     <Route path={'/new_password'} element={<NewPassword/>}/>
                     <Route path={'/test'} element={<Test/>}/>
--- src/common/components/Layout/Layout.tsx
+++ src/common/components/Layout/Layout.tsx
@@ -3,12 +3,14 @@ import {Link, Outlet} from "react-router-dom";
 
 const Layout = () => {
 
+
     return (
         <>
             <header>
-                <div><Link to="/login">login</Link></div>
+
+                <div><Link to="/sign-in">login</Link></div>
                 <div><Link to="/profile">profile</Link></div>
-                <div><Link to="/registration">registration</Link></div>
+                <div><Link to="/sign-up">registration</Link></div>
                 <div><Link to="/res_password">res_password</Link></div>
                 <div><Link to="/new_password">new_password</Link></div>
                 <div><Link to="/404">404</Link></div>
