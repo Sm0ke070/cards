2ed3d479c69c424bf370ee40d45f812b7fe56a09
Ilya Lunin <lunia56@mail.ru>
2023-02-07 09:37:51 +0300

    test - Ilya

--- src/common/components/super-components/Test.tsx
+++ src/common/components/super-components/Test.tsx
@@ -6,10 +6,16 @@ import SuperCheckbox from "./SuperCheckbox/SuperCheckbox";
 const Test = () => {
     return (
         <div>
+            <p>1</p>
+            <p>2</p>
+            <p>3</p>
             <h1>Test</h1>
             <SuperButton/>
             <SuperInputText/>
             <SuperCheckbox/>
+            <p>4</p>
+            <p>5</p>
+            <p>6</p>
         </div>
     );
 };
