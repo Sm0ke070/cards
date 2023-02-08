48ad9fd352bcf946bd96b981feab2252ca40b8d0
Ilya Lunin <lunia56@mail.ru>
2023-02-08 19:38:17 +0300

    add Progress

--- src/features/auth/sign-up/signUp-reducer.ts
+++ src/features/auth/sign-up/signUp-reducer.ts
@@ -2,6 +2,7 @@ import {Dispatch} from 'redux';
 import axios, {AxiosError} from 'axios';
 import {signUpAPI, SignUpParamsType} from './signUp.api';
 import {ActionsType} from '../../../app/store';
+import {setAppStatusAC, SetAppStatusActionType} from '../../../app/app-reducer';
 
 type initialStateType = {
     errorSignUp: null | string
@@ -25,28 +26,36 @@ export const signUpReducer = (state: initialStateType = initialState, action: Si
 }
 
 
-export const setErrorSignUpAC = (errorSignUp: string) =>
+export const setErrorSignUpAC = (errorSignUp: string|null) =>
     ({type: 'signUp/SET-IS-ERROR-SIGN-UP', errorSignUp} as const)
 export const setIsRegisteredAC = (isRegistered: boolean) =>
     ({type: 'signUp/SET-IS-REGISTERED', isRegistered} as const)
 
 
 export const SignUpTC = (data: SignUpParamsType) => async (dispatch: Dispatch<ActionsType>) => {
-    console.log('data', data)
+    dispatch(setAppStatusAC('loading'))
     try {
         const res = await signUpAPI.register(data)
-        if (res) {
-            dispatch(setIsRegisteredAC(true))
+        console.log(res)
+        if (res.statusText==='Created') {
+            setTimeout(()=>{
+                dispatch(setIsRegisteredAC(true))
+            },4000)
+            dispatch(setAppStatusAC('succeeded'))
+
         } else {
+            dispatch(setAppStatusAC('failed'))
 
         }
     } catch (e) {
+        dispatch(setAppStatusAC('failed'))
         const err = e as Error | AxiosError<{ error: string }>
         if (axios.isAxiosError(err)) {
             const error = err.response?.data ? err.response.data.error : err.message
             dispatch(setErrorSignUpAC(error))
         }
+
     }
 }
 
-export type SignUpActionsType = ReturnType<typeof setErrorSignUpAC> | ReturnType<typeof setIsRegisteredAC>
+export type SignUpActionsType = ReturnType<typeof setErrorSignUpAC> | ReturnType<typeof setIsRegisteredAC>|SetAppStatusActionType
