a0ba01174e4cb4442b78ee12ce7277bc870f9cb1
Ilya Lunin <lunia56@mail.ru>
2023-02-16 11:50:10 +0300

    add Packs table-1

--- src/features/Packs/Packs.tsx
+++ src/features/Packs/Packs.tsx
@@ -3,27 +3,28 @@ import {ColumnsType} from 'antd/es/table';
 import {Table} from 'antd';
 import {useAppDispatch, useAppSelector} from '../../app/store';
 import {getPacks} from './packsReducer';
+import {toFormData} from 'axios';
+import {formatDate} from '../../common/utils/formatDate';
 
 export const Packs = () => {
-const dispatch = useAppDispatch()
-    const packs = useAppSelector(state=>state.packs.cardPacks)
-    const page = useAppSelector(state=>state.packs.queryParams.page)
-    const packName = useAppSelector(state=>state.packs.queryParams.packName)
-    const pageCount = useAppSelector(state=>state.packs.queryParams.pageCount)
-    const userId = useAppSelector(state=>state.packs.queryParams.user_id)
-    const min = useAppSelector(state=>state.packs.queryParams.min)
-    const max = useAppSelector(state=>state.packs.queryParams.max)
-    const sortPacks = useAppSelector(state=>state.packs.queryParams.sortPacks)
-    useEffect(()=>{
+    const dispatch = useAppDispatch()
+    const packs = useAppSelector(state => state.packs.cardPacks)
+    const page = useAppSelector(state => state.packs.queryParams.page)
+    const packName = useAppSelector(state => state.packs.queryParams.packName)
+    const pageCount = useAppSelector(state => state.packs.queryParams.pageCount)
+    const userId = useAppSelector(state => state.packs.queryParams.user_id)
+    const min = useAppSelector(state => state.packs.queryParams.min)
+    const max = useAppSelector(state => state.packs.queryParams.max)
+    const sortPacks = useAppSelector(state => state.packs.queryParams.sortPacks)
+    useEffect(() => {
         dispatch(getPacks())
-    },[page, packName, pageCount, userId, min, max, sortPacks])
-    console.log(packs)
+    }, [page, packName, pageCount, userId, min, max, sortPacks])
 
     interface DataType {
-        key: React.Key;
         name: string;
-        age: number;
-        address: string;
+        cardsCount: number;
+        lastUpdated: string
+        userName: string
     }
 
     const columns: ColumnsType<DataType> = [
@@ -31,43 +32,42 @@ const dispatch = useAppDispatch()
             title: 'Name',
             width: 110,
             dataIndex: 'name',
-            key: 'name',
             fixed: 'left',
         },
         {
             title: 'Cards',
             width: 100,
-            dataIndex: 'Cards',
-            key: 'Cards',
-            fixed: 'left',
+            dataIndex: 'cardsCount',
         },
 
         {
-            title: 'Last Updated ',
-            dataIndex: 'address',
-            key: '1',
+            title: 'Last Updated',
+            dataIndex: 'lastUpdated',
             width: 150,
         },
         {
             title: 'Created by',
-            dataIndex: 'address',
-            key: '2',
+            dataIndex: 'userName',
             width: 150,
         },
         {
             title: 'Action',
-            key: 'operation',
             fixed: 'right',
             width: 100,
             render: () => <a>action</a>,
         },
     ];
-    const data = packs.map((el)=>{
-
-})
+    const data = packs.map((p) => {
+        return {
+            name: p.name,
+            cardsCount: p.cardsCount,
+            lastUpdated: formatDate(p.updated),
+            userName: p.user_name
+        }
+    })
 
     return <>
 
-        {/*<Table columns={columns} dataSource={data} scroll={{x: 1500, y: 300}}/>*/}
+        <Table columns={columns} dataSource={data} scroll={{x: 1500, y: 300}}/>
     </>
 }
