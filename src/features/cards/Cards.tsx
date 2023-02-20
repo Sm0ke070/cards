ba54d8b57b2eca97ec886c7643d0ae14a19a3aa4
Sm0ke070 <smokedeveloper070@gmail.com>
2023-02-20 23:59:46 +0300

    Nikita-cards

--- src/features/cards/Cards.tsx
+++ src/features/cards/Cards.tsx
@@ -1,18 +1,111 @@
-import React from 'react';
-import {Link} from 'react-router-dom';
+import React, {useEffect} from 'react';
+import {useNavigate} from 'react-router-dom';
 import {routes} from '../../constants/constants';
-import {Button} from 'antd';
+import {useAppDispatch, useAppSelector} from "../../app/store";
+import {ColumnsType} from "antd/es/table";
+import {SortPackName} from "../packs/packs-sort/SortPackName";
+import {SortPacksCards} from "../packs/packs-sort/SortPacksCards";
+import {SortPacksUpdated} from "../packs/packs-sort/SortPacksUpdated";
+import {SortPackCreatedBy} from "../packs/packs-sort/SortPackCreatetBy";
+import {formatDate} from "../../common/utils/formatDate";
+import {Actions} from "../packs/Actions";
+import {getCard} from "./cardsReducer";
+import s from "../packs/Packs.module.css";
+import {PacksHead} from "../packs/PacksHead";
+import {PacksSettings} from "../packs/PackSettings/PacksSettings";
+import {Table} from "antd";
+import {setPacksPageAC, setPageCountAC} from "../packs/packsReducer";
 
-export  const Cards = () => {
+interface DataType {
+    key: React.Key
+    question: string;
+    answer: number;
+    lastUpdated: string
+    grade: string
+}
 
-   const  onClickHandler = ()=>{
+export const Cards = () => {
+    const navigate = useNavigate()
+    const isLoggedIn = useAppSelector(state => state.auth.isLoggedIn)
+    const userId = useAppSelector(state => state.auth.userData._id)
+    const card = useAppSelector(state => state.packs.cardPacks)
+    const isLoading = useAppSelector(state => state.app.status)
+    const packName = useAppSelector(state => state.packs.queryParams.packName)
+    const dispatch = useAppDispatch()
 
-   }
+    useEffect(() => {
+        //dispatch(getCard())
+    })
+    const data = card.map((c) => {
+        return {
+            key: c._id,
+            name: c.name,
+            cardsCount: c.cardsCount,
+            lastUpdated: formatDate(c.updated),
+            userName: c.user_name,
+            actions: <Actions packId={c._id} packUserId={c.user_id}/>
+
+        }
+    })
+
+    const columns: ColumnsType<any> = [
+        {
+            title: <SortPackName/>,
+            dataIndex: 'name',
+            width: 350,
+
+        },
+        {
+            title: <SortPacksCards/>,
+            dataIndex: 'cardsCount',
+            width: 150,
+        },
+
+        {
+            title: <SortPacksUpdated/>,
+            dataIndex: 'lastUpdated',
+            width: 150,
+        },
+        {
+            title: <SortPackCreatedBy/>,
+            dataIndex: 'userName',
+            width: 150,
+        },
+        {
+            title: 'Action',
+            dataIndex: 'actions',
+            width: 150,
+        },
+    ];
+
+    if (!isLoggedIn) {
+        navigate(routes.SIGN_IN)
+    }
+
     return (
-        <div>
-            <Link to={routes.PACKS} >Back to Packs</Link>
-            Hello World
-            <Button onClick={onClickHandler}>Ajax Request</Button>
+        <div className={s.tableWrapper}>
+            <PacksHead/>
+            <PacksSettings/>
+            <div>
+
+                {
+                    <Table columns={columns} dataSource={data} scroll={{x: 1000, y: 500}} loading={isLoading === 'loading'}
+                           onRow={record => {
+                               return {
+
+                               }
+                           }} pagination={{
+                        current: 1,
+                        pageSize: 1,
+                        total: 1,
+                        position: ['bottomLeft'],
+                        onChange: (page, pageSize) => {
+                            dispatch(setPacksPageAC(page))
+                            dispatch(setPageCountAC(pageSize))
+                        },
+                    }}/>
+                }
+            </div>
         </div>
     );
 };
--- src/features/cards/cardsReducer.ts
+++ src/features/cards/cardsReducer.ts
@@ -1,52 +1,59 @@
 import {sortingCardsMethods} from '../../constants/sortingMethods';
 import {Dispatch} from 'redux';
+import {setAppStatusAC} from "../../app/AppReducer";
+import {AppRootStateType} from "../../app/store";
+import {cardAPI} from "./cardAPI";
 
-//
-// const initialState = {
-//     cards: [] as cardsType[],
-//     cardPacksTotalCount: 0,
-//     minCardsCount: 0,
-//     maxCardsCount: 110,
-//     resetFilter: false,
-//     queryParams: {
-//         pageCount: 5,
-//         page: 1,
-//         cardName: '',
-//         sortCards: sortingCardsMethods.desUpdate
-//     },
-// }
-// type InitialStateType = typeof initialState
-//
-// export const cardsReducer = (state: InitialStateType = initialState, action: packsReducerActionsType): InitialStateType => {
-//     switch (action.type) {
-//
-//         default:
-//             return state
-//     }
-// }
-// //Actions
-//
-//
-//
-// // Thunks
-// export const getCards = () => async (dispatch: Dispatch, getState: any) => {
-//     const {packName, sortPacks, max, min, page, pageCount} = getState().c.queryParams
-//     const user_id = filter === 'MY' ? getState().auth.userData._id : ''
-//     dispatch(setAppStatusAC('loading'))
-//
-//     try {
-//         const res = await PacksAPI.getPacks({packName, sortPacks, max, min, page, pageCount, user_id})
-//         const {cardPacks, cardPacksTotalCount, minCardsCount, maxCardsCount} = res.data
-//         dispatch(setPacks({cardPacks, cardPacksTotalCount, minCardsCount, maxCardsCount}))
-//         dispatch(setAppStatusAC('succeeded'))
-//     } catch (e) {
-//         dispatch(setAppStatusAC('failed'))//временно тут
-//
-//     }
-//
-// }
-//
+export type CardType = {
+    answer: string
+    question: string
+    cardsPack_id: string
+    grade: number
+    shots: number
+    user_id: string
+    created: string
+    updated: string
+    _id: string
+}
+
+const initialState = {
+    cards: [] as CardType[],
+    cardsTotalCount: 0,
+    maxGrade: 0,
+    minGrade: 0,
+    page: 0,
+    pageCount: 0,
+    packUserId: '',
+    resetFilter: false,
+    queryParams: {
+        pageCount: 5,
+        page: 1,
+        cardName: '',
+        sortCards: sortingCardsMethods.desUpdate
+    },
+}
 
 
+type InitialStateType = typeof initialState
 
-// Types
+export const cardsReducer = (state: InitialStateType = initialState, action: any): InitialStateType => {
+    switch (action.type) {
+
+        default:
+            return state
+    }
+}
+
+
+export const getCard = () => async (dispatch: Dispatch, getState: () => AppRootStateType) => {
+    //const {_id} = getState().packs.cardPacks
+
+    try {
+        //const res = await cardAPI.getCard()
+    } catch (e) {
+        dispatch(setAppStatusAC('failed'))//временно тут
+
+    }
+
+}
+
